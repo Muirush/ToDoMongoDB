449316d6bcc856d9a815ad8bfadc32e2c78f001b
Muirush <rapturesimon@gmail.com>
2022-06-08 19:03:57 +0300

    work in progress

---
+++ app/src/main/java/com/galoppingtech/todomongodb/UI/Login.kt
@@ -0,0 +1,9 @@
+package com.galoppingtech.todomongodb.UI
+
+import androidx.fragment.app.Fragment
+import com.galoppingtech.todomongodb.R
+
+
+class login : Fragment(R.layout.fragment_login) {
+
+}
--- app/src/main/java/com/galoppingtech/todomongodb/UI/ViewNotes.kt
+++ app/src/main/java/com/galoppingtech/todomongodb/UI/ViewNotes.kt
@@ -1,7 +1,8 @@
-package com.galoppingtech.todomongodb
+package com.galoppingtech.todomongodb.UI
 
 
 import androidx.fragment.app.Fragment
+import com.galoppingtech.todomongodb.R
 
 class ViewNotes : Fragment(R.layout.fragment_view_notes) {
 
