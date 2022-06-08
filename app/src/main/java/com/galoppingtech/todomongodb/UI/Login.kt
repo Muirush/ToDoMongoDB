36f68f80982e93a1a92fc85ec6362df492fa2800
Muirush <rapturesimon@gmail.com>
2022-06-08 20:51:07 +0300

    Progressive

--- app/src/main/java/com/galoppingtech/todomongodb/UI/Login.kt
+++ app/src/main/java/com/galoppingtech/todomongodb/UI/Login.kt
@@ -1,9 +1,49 @@
 package com.galoppingtech.todomongodb.UI
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.galoppingtech.todomongodb.R
+import com.galoppingtech.todomongodb.databinding.FragmentLoginBinding
 
 
 class login : Fragment(R.layout.fragment_login) {
+    private var _binding :FragmentLoginBinding ?= null
+    private  val binding get() = _binding !!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentLoginBinding.bind(view)
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.lRegister.setOnClickListener(){
+            findNavController().navigate(R.id.register)
+        }
+
+        binding.materialButton.setOnClickListener(){
+            val name = binding.logUsername.text.toString().trim()
+            val pwd = binding.logPassword.text.toString().trim()
+            if(name.isEmpty()&& pwd.isEmpty()){
+                binding.apply {
+                    logUsername.error = "cannot be empty"
+                    logPassword.error = "Cannot be empty"
+                }
+
+            }
+            else{
+                loginMethod()
+
+            }
+        }
+
+    }
+
+    private fun loginMethod() {
+        Toast.makeText(activity,"Progressing well", Toast.LENGTH_SHORT).show()
+    }
 
 }
--- app/src/main/java/com/galoppingtech/todomongodb/UI/createNote.kt
+++ app/src/main/java/com/galoppingtech/todomongodb/UI/createNote.kt
@@ -3,7 +3,7 @@ package com.galoppingtech.todomongodb.UI
 import androidx.fragment.app.Fragment
 import com.galoppingtech.todomongodb.R
 
-
+//
 class createNote : Fragment(R.layout.fragment_create_note) {
 
 }
